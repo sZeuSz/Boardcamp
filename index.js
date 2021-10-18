f4eb109d59017d6a9f7c0dea0e5d3c99c2b5f720
zeus.k99@gmail.com <zeus.k99@gmail.com>
2021-10-18 14:37:49 -0400

    customers routes add

--- index.js
+++ index.js
@@ -136,3 +136,130 @@ app.post('/games', async (req, res) => {
 
 })
 
+// Customers Routes
+
+app.get('/customers', async (req, res) => {
+
+    const {cpf} = req.query;
+
+    try{
+
+        if(cpf){
+            const searchCustoumerCpfMatch = await connection.query('SELECT * FROM customers WHERE cpf LIKE $1;', [`${cpf}%`]);
+
+            return res.send(searchCustoumerCpfMatch.rows);
+        }
+
+        const customers = await connection.query('SELECT * FROM customers;');
+
+
+        return res.send(customers.rows);
+    }
+    catch (error){
+
+        return res.sendStatus(500);
+    }
+})
+
+app.get('/customers/:id', async (req, res) => {
+
+    const {id} = req.params;
+
+    try{
+        
+        const customerById = await connection.query('SELECT * FROM customers WHERE id=$1;', [id])
+
+        if(!customerById.rows.length){
+
+            return res.sendStatus(404);
+        }
+
+        return res.send(customerById.rows);
+    }
+    catch (error){
+
+        return res.sendStatus(500);
+    }
+
+})
+
+app.post('/customers', async (req, res) => {
+
+    const{
+        name,
+        phone,
+        cpf,
+        birthday
+    } = req.body;
+    
+    let validCpf = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
+    let validDate = /^([0-9]{4}[-/]?((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-/]?02[-/]?29)$/;
+    let validPhone = /[0-9]{11}|[0-9]{10}/;
+
+    if(!validCpf.test(cpf) || cpf.length !== 11 || !validPhone.test(phone) || (phone.length !== 10 && phone.length !== 11) || !name.length || !validDate.test(birthday) ){
+
+        return res.sendStatus(400)
+    }
+
+    try{
+        const cpfCheck = await connection.query('SELECT * FROM customers WHERE cpf= $1', [cpf]);
+
+        if(cpfCheck.rows.length){
+
+            return res.sendStatus(409);
+        }
+
+        const insertCustomer = await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday])
+
+        return res.sendStatus(201)
+    }
+    catch (error){
+
+        return res.sendStatus(500)
+    }
+})
+
+app.put('/customers/:id', async (req, res) => {
+
+    const { id } = req.params;
+
+    const {
+        name,
+        phone,
+        cpf,
+        birthday
+    } = req.body;
+
+    let validCpf = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
+    let validDate = /^([0-9]{4}[-/]?((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-/]?02[-/]?29)$/;
+    let validPhone = /[0-9]{11}|[0-9]{10}/;
+
+    if(!validCpf.test(cpf) || cpf.length !== 11 || !validPhone.test(phone) || (phone.length !== 10 && phone.length !== 11) || !name.length || !validDate.test(birthday) ){
+
+        return res.sendStatus(400)
+    }
+
+    try {
+        const customerById = await connection.query('SELECT * FROM customers WHERE id=$1', [id]);
+        
+        if(!customerById.rows.length){
+
+            return res.sendStatus(404);
+        }
+
+        const customerByCpf = await connection.query('SELECT * FROM customers WHERE cpf=$1', [cpf]);
+
+        if(customerByCpf.rows.length){
+
+            return res.sendStatus(409)
+        }
+
+        const updateCustomer = await connection.query('UPDATE customers SET (name, phone, cpf, birthday) = ($1, $2, $3, $4) WHERE id=$5', [name, phone, cpf, birthday, id]);
+
+        return res.sendStatus(200)
+    }
+    catch (error){
+
+        return res.sendStatus(500);
+    }
+})
