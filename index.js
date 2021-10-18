076ff362cfbfd9998c00290ddf833434ff0e1d4a
zeus.k99@gmail.com <zeus.k99@gmail.com>
2021-10-18 16:30:15 -0400

    Adicionando get sem query :D

--- index.js
+++ index.js
@@ -300,6 +300,16 @@ app.get('/rentals', async (req, res) => {
                         JOIN categories ON categories.id = games."categoryId" WHERE rentals."gameId" = $1`, [gameId])
             return res.send(listRentalsG.rows);
         }
+        else{
+            const listRentals = await connection.query(`SELECT rentals.*, jsonb_build_object('id', customers.id, 'name', customers.name) AS customer,
+                jsonb_build_object('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game
+                    FROM rentals
+                        JOIN customers ON rentals."customerId" = customers.id
+                        JOIN games ON rentals."gameId" = games.id
+                        JOIN categories ON categories.id = games."categoryId"`)
+           
+            return res.send(listRentals.rows);
+        }
     } 
     catch (error){
 
